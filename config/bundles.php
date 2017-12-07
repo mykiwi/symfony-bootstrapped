8e55868493d399f0359b8e89e320afedcfe05d62
Romain Gautier <mail@romain.sh>
2017-12-07 17:48:33 +0100

    add twig

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
