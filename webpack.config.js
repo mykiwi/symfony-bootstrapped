bf03ffb3c07f1a51c90edb8ee567c8752cc9ebe7
Romain Gautier <mail@romain.sh>
2017-12-08 15:56:10 +0100

    add js content

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ Encore
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/app', './assets/js/app.js')
     // .addStyleEntry('css/app', './assets/css/app.scss')
 
     // uncomment if you use Sass/SCSS files
