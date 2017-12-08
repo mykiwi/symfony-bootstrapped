1eb3d338776d86d772ce82c487bfc40a1798d538
Romain Gautier <mail@romain.sh>
2017-12-08 16:04:54 +0100

    add fixtures (doctrine-fixtures-bundle for now)

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
