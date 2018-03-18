8586c443d065192fc418af97cd3d4e13e6b699c7
Romain Gautier <mail@romain.sh>
2018-03-18 14:23:27 +0100

    fix cs

--- src/Entity/Pizza.php
+++ src/Entity/Pizza.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the appname project.
+ *
+ * (c) Romain Gautier <mail@romain.sh>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
--- src/Migrations/Version20171208151336.php
+++ src/Migrations/Version20171208151336.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the appname project.
+ *
+ * (c) Romain Gautier <mail@romain.sh>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
