4f0fa543292491e8218e51471e2e98b3c8deab1e
Thomas Bisignani <thomas.bisignani@sensiolabs.com>
2018-04-03 00:19:15 +0200

    Fixed migrations and removed uselesses gitignore files

--- src/DataFixtures/PizzaFixtures.php
+++ src/DataFixtures/PizzaFixtures.php
@@ -11,18 +11,18 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Foo;
+use App\Entity\Pizza;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class FooFixtures extends Fixture
+class PizzaFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         for ($i = 0; $i < 20; ++$i) {
-            $foo = new Foo();
-            $foo->setBar('bar '.$i);
-            $manager->persist($foo);
+            $pizza = new Pizza();
+            $pizza->setName('Pizza '.$i);
+            $manager->persist($pizza);
         }
 
         $manager->flush();
