a617830116bff3d0147ec415f6f6eed92570b053
Romain Gautier <github@romain.sh>
2022-05-04 19:02:46 +0000

    add doctrine packages

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
